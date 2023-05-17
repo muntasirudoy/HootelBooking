37e93cf0fc6e52ad1a0e4bf0652196f89f3f5d4e
muntasir <muntasirudoy4@gmaiul.com>
2023-05-17 20:43:18 +0600

    first

---
+++ css/index.js
@@ -0,0 +1,25 @@
+
+const [state, setState] = useState("")
+
+
+
+const projects = [
+    {
+        id: 1
+    },
+    {
+        id: 1
+    },
+    {
+        id: 1
+    },
+    {
+        id: 1
+    },
+    {
+        id: 1
+    },
+]
+
+
+state.name
